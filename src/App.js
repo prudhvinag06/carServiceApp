fe4dfba252a38f4703f47a53aac04e4b72ae340b
prudhvinag06 <prudhvinag637@gmail.com>
2022-03-29 21:16:55 +0530

    basic html + css in react

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="container">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +17,31 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <h1>Register</h1>
+        <p>Please fill in this form to create an account.</p>
+      <form action="action_page.php">
+        <div class="container signin">
+          <p>Already have an account? <a href="#">Sign in</a>.</p>
+        </div>
+        <hr></hr>
+        <label for="email"><b>Email</b></label>
+        <input type="text" placeholder="Enter Email" name="email" id="email" required></input>
+
+        <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" id="psw" required></input>
+
+        <label for="psw-repeat"><b>Repeat Password</b></label>
+        <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required></input>
+        <hr></hr>
+
+        <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
+        <button type="submit" class="registerbtn">Register</button>
+        
+
+      </form>
+
+
     </div>
   );
 }
