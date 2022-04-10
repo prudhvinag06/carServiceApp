f3338f302dee1aa8da21d180835cdb35ed4a1c95
prudhvinag06 <prudhvinag637@gmail.com>
2022-04-10 19:14:12 +0530

    added spring api for services

---
+++ carServices/src/main/java/com/example/carServices/carServices/CarRestServices.java
@@ -0,0 +1,19 @@
+package com.example.carServices.carServices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CarRestServices {
+    @Autowired
+    private ServicesHardCoded servicesHardCoded;
+
+    @GetMapping("/users/city/services/getAllServices")
+    public List<Services> getAllServices(){
+        return servicesHardCoded.getServices();
+    }
+}
---
+++ carServices/src/main/java/com/example/carServices/carServices/ServicesHardCoded.java
@@ -0,0 +1,21 @@
+package com.example.carServices.carServices;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class ServicesHardCoded {
+    private static List<Services> services = new ArrayList();
+    private static long idCounter = 0;
+    static {
+        services.add(new Services(++idCounter, "Service 1", "Bangalore", true));
+        services.add(new Services(++idCounter, "Service 2", "Hyderabad", true));
+        services.add(new Services(++idCounter, "Service 3", "Pathankot", true));
+    }
+    public List<Services> getServices(){
+        return services;
+    }
+}
