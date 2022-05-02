3c3b75c66f91f96cb1d049f69f9199b5942aecaf
prudhvinag06 <prudhvinag637@gmail.com>
2022-05-02 12:16:37 +0530

    not so sure commit. If bugs arises revert to previous version

--- carServices/src/main/java/com/example/carServices/Controller.java
+++ carServices/src/main/java/com/example/carServices/Controller.java
@@ -9,7 +9,7 @@ import java.util.List;
 @RestController
 @CrossOrigin(origins = "http://localhost:3000/")
 
-public class CarRestServices {
+public class Controller {
     @Autowired
     private ServicesHardCoded servicesHardCoded;
 
